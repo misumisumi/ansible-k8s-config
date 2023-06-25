f92621f0a4d8fb2bfafa9e555fd99e9a5a041c93
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-26 08:42:12 +0900

    fix: ansibleのオーバーライド

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   description = "Playbooks for apps of my k8s cluster";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.devshell.url = "github:numtide/devshell";
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
+        inputs.devshell.flakeModule
       ];
       systems = ["x86_64-linux"];
       perSystem = {
@@ -16,26 +18,47 @@
         system,
         ...
       }: let
-        _pkgs = with pkgs; [
-          bashInteractive
-          jq
-          yq # python-yq
-          argocd
-          ansible
-          kubectl
-          kubernetes-helm
-        ];
+        myScripts = pkgs.callPackage (import ./scripts) {};
+        ANSIBLE_COLLECTIONS_PATHS = pkgs.callPackage ./collections {};
       in {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [];
+          overlays = [
+            (final: prev: {
+              python3Packages = prev.python3Packages.override {
+                overrides = pfinal: pprev: {
+                  ansible-core = pprev.ansible-core.overridePythonAttrs (old: {
+                    buildInputs = (old.buildInputs or []) ++ [pprev.kubernetes];
+                    makeWrapperArgs = (old.makeWrapperArgs or []) ++ ["--set PYTHONPATH $PYTHONPATH"];
+                  });
+                };
+              };
+            })
+          ];
           config.allowUnfree = true;
         };
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = _pkgs;
-          buildInputs = [];
-          shellHooks = ''
-          '';
+        devshells.default = {
+          env = [
+            {
+              name = "ANSIBLE_COLLECTIONS_PATHS";
+              value = ANSIBLE_COLLECTIONS_PATHS;
+            }
+          ];
+          commands = [];
+          packages = with pkgs;
+          with myScripts; [
+            bashInteractive
+            jq
+            yq # python-yq
+            argocd
+            ansible
+            kubectl
+            kubernetes-helm
+            nvfetcher
+            # MyScripts
+            k
+            he
+          ];
         };
       };
     };
